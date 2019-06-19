fe96e52096f4e2852722efd696c6e93a36808f89
OUghTMS <misha.saan@gmail.com>
2019-06-19 19:51:00 +0300

    final7

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
   render() {
     const swch = this.state.isOpen && <LeftBar />
     return (
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <div className="app">
           {swch}
           <RightSec open={this.state.isOpen} hide={this.hideButton}/>
